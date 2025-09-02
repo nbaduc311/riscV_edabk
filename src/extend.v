1520e22b715766a98de66919fa165144e01b2ae8
nbaduc311 <124475308+nbaduc311@users.noreply.github.com>
2025-09-03 01:19:22 +0700

    add u type

--- src/extend.v
+++ src/extend.v
@@ -6,10 +6,20 @@ module extend(
 
     always @(*) begin
         case (immsrc)
-            2'b00: immext = {{20{instr[31]}}, instr[31:20]}; // I-type
-            2'b01: immext = {{20{instr[31]}}, instr[31:25], instr[11:7]}; // S-type (stores)
-            2'b10: immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0}; // B-type (branches)
-            2'b11: immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0}; // J-type (jal)
+            // I-type (loads, ALU immediate, jalr)
+            3'b000: immext = {{20{instr[31]}}, instr[31:20]}; 
+
+            // S-type (stores)
+            3'b001: immext = {{20{instr[31]}}, instr[31:25], instr[11:7]}; 
+
+            // B-type (branches)
+            3'b010: immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+
+            // J-type (jal)
+            3'b011: immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+            
+            // U-type (lui, auipc)
+            3'b100: immext = {instr[31:12], 12'b0};
             default: immext = 32'bx; // undefined
         endcase
     end
